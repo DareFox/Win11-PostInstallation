f7fbab8074198d01534f518965cae861f0fe2bf1
DareFox <easymalink@gmail.com>
2022-12-28 02:41:45 +0300

    Install-Spotify-Adblock: Dont use aliases

--- InstallSpotifyAdblock.ps1
+++ InstallSpotifyAdblock.ps1
@@ -1,4 +1,4 @@
 function Install-Spotify-Adblock {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; 
-    iex "& { $((iwr -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content) } -confirm_spoti_recomended_over -podcasts_on -cache_off -block_update_off -start_spoti"
+    Invoke-Expression "& { $((Invoke-WebRequest -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content) } -confirm_spoti_recomended_over -podcasts_on -cache_off -block_update_off -start_spoti"
 }
