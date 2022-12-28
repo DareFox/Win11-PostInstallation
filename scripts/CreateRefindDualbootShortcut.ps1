8004bf536e9353002cb0b88130f135425d1c0942
DareFox <easymalink@gmail.com>
2022-12-28 14:51:31 +0300

    Script: Add refind dual boot shorcut

---
+++ scripts/CreateRefindDualbootShortcut.ps1
@@ -0,0 +1,23 @@
+function Create-Refind-Dualboot-Shortcut {
+    $folder = New-Item "C:\DualbootRefind" -ItemType Directory
+    $baseUrl = "https://raw.githubusercontent.com/DareFox/Win11-PostInstallation/main/resources/"
+    $files = "RefindNextBoot.cs","RefindNextBoot.ps1","dualboot.ps1","dualboot.bat"
+
+    $ProgressPreference = 'SilentlyContinue' # https://stackoverflow.com/a/43477248
+
+    foreach ($element in $files) {
+        Invoke-WebRequest -URI "$baseUrl$element" -OutFile "$folder\$element" 
+    }
+
+    $ShortcutPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Dualboot.lnk"
+    $TargetPath = "$folder\dualboot.bat"
+
+    $WScriptObj = New-Object -ComObject ("WScript.Shell")
+    $ShortcutObj = $WscriptObj.CreateShortcut($ShortcutPath)
+    $ShortcutObj.TargetPath = $TargetPath
+    $ShortcutObj.Save()
+
+    $bytes = [System.IO.File]::ReadAllBytes("$Home\Desktop\ColorPix.lnk")
+    $bytes[0x15] = $bytes[0x15] -bor 0x20 # Run as Administrator
+    $bytes | Set-Content $ShortcutPath -Encoding Byte
+}
